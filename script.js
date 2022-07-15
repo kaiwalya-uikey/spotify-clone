fc6cd0a5428b4f37523b520cea586a0968afefff
monty <kaiwalyauikey5@gmail.com>
2022-07-15 14:10:00 +0530

    im a billioniare

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ let songs = [ // array of objects hai ye
     {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
     {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Luna - Diljit Dosanjh", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "No Love - Shubh", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Old skool - Sidhu Moosewala", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "We Rollin - Shubh", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
